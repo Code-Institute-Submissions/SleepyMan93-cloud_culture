d1a219adf382e11fc1b0f0889b21265923b972df
SleepyMan93 <w-donovan@hotmail.co.uk>
2021-11-11 22:53:16 +0000

    Added adjust bag and remove from bag views with calc subtotal in shopping bag

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_bag(request):
@@ -32,3 +32,49 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+def adjust_bag(request, item_id):
+    """ Adjust the quantity of the particular product to the shipping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    nicotine = None 
+    if 'nicotine_level' in request.POST:
+        nicotine = request.POST['nicotine_level']
+    bag = request.session.get('bag', {})
+
+    if nicotine:
+        if quantity > 0:
+            bag[item_id]['items_by_nicotine'][nicotine] = quantity
+        else:
+            del bag[item_id]['items_by_nicotine'][nicotine]
+            if not bag[item_id]['items_by_nicotine']:
+                bag.pop(item_id)           
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+def remove_from_bag(request, item_id):
+    """ Remove item from the shipping bag """
+
+    try:
+        nicotine = None 
+        if 'nicotine_level' in request.POST:
+            nicotine = request.POST['nicotine_level']
+        bag = request.session.get('bag', {})
+
+        if nicotine:
+                del bag[item_id]['items_by_nicotine'][nicotine]
+                if not bag[item_id]['items_by_nicotine']:
+                    bag.pop(item_id)
+        else:
+                bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
