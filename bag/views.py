ee091c679eaa4602b4b1a30a3ef5fa23e542aba9
SleepyMan93 <w-donovan@hotmail.co.uk>
2021-11-12 00:43:39 +0000

    Implemented bag notification CSS, shopping bag preview and additional messages. Still need to get close toast button to work

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -12,7 +12,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of a particular product to the shopping bag """
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     nicotine = None 
@@ -24,16 +24,20 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if nicotine in bag[item_id]['items_by_nicotine'].keys():
                 bag[item_id]['items_by_nicotine'][nicotine] += quantity
+                messages.success(request, f'Updated {nicotine} {product.name} quantity to {bag[item_id]["items_by_nicotine"][nicotine]} ')
             else:
                 bag[item_id]['items_by_nicotine'][nicotine] = quantity
+                messages.success(request, f'Added {nicotine} {product.name} to your bag') 
         else:
-            bag[item_id] = {'items_by_nicotine': {nicotine: quantity}}    
+            bag[item_id] = {'items_by_nicotine': {nicotine: quantity}}
+            messages.success(request, f'Added {nicotine} {product.name} to your bag')    
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
+            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
         else:
             bag[item_id] = quantity
-            messages.error(request, f'Added {product.name} to your bag')
+            messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
@@ -41,6 +45,7 @@ def add_to_bag(request, item_id):
 def adjust_bag(request, item_id):
     """ Adjust the quantity of the particular product to the shipping bag """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     nicotine = None 
     if 'nicotine_level' in request.POST:
@@ -50,15 +55,19 @@ def adjust_bag(request, item_id):
     if nicotine:
         if quantity > 0:
             bag[item_id]['items_by_nicotine'][nicotine] = quantity
+            messages.success(request, f'Updated {nicotine} {product.name} quantity to {bag[item_id]["items_by_nicotine"][nicotine]} ')
         else:
             del bag[item_id]['items_by_nicotine'][nicotine]
             if not bag[item_id]['items_by_nicotine']:
-                bag.pop(item_id)           
+                bag.pop(item_id)
+            messages.success(request, f'Removed {nicotine} {product.name} from your bag')            
     else:
         if quantity > 0:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
         else:
             bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -67,19 +76,24 @@ def remove_from_bag(request, item_id):
     """ Remove item from the shipping bag """
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         nicotine = None 
         if 'nicotine_level' in request.POST:
             nicotine = request.POST['nicotine_level']
         bag = request.session.get('bag', {})
 
         if nicotine:
-                del bag[item_id]['items_by_nicotine'][nicotine]
-                if not bag[item_id]['items_by_nicotine']:
-                    bag.pop(item_id)
-        else:
+            del bag[item_id]['items_by_nicotine'][nicotine]
+            if not bag[item_id]['items_by_nicotine']:
                 bag.pop(item_id)
+            messages.success(request, f'Removed {nicotine} {product.name} from your bag')
+        else:
+            bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
+
     except Exception as e:
+        messages.error(request, f'Error removing item: (e)')
         return HttpResponse(status=500)
