e8bfa982812ab96908b3ffebb5c80912695ae817
SleepyMan93 <w-donovan@hotmail.co.uk>
2021-11-13 16:49:50 +0000

    Added webhooks.py and updated project settings

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -11,5 +11,22 @@ class StripeWH_Handler:
         Handle a generic/unknown or unexpected webhook event 
         """
         return HttpResponse(
+            content=f'Unhandled Webhook recieved: {event["type2"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle the payment_intent.succeed webhook from Stripe 
+        """
+        return HttpResponse(
             content=f'Webhook recieved: {event["type2"]}',
             status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle the payment_intent.payment_failed webhook from Stripe 
+        """
+        return HttpResponse(
+            content=f'Webhook recieved: {event["type2"]}',
+            status=200)
+
