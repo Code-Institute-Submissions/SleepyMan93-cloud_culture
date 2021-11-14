1750d663cc3e61f550423f7fb411d2efdefb7034
SleepyMan93 <w-donovan@hotmail.co.uk>
2021-11-14 16:09:26 +0000

    Fixed admin date error. Order date now shows in Admin panel

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 from .models import Order, OrderPerItem
 from products.models import Product
@@ -7,12 +10,28 @@ from profiles.models import UserProfile
 import json
 import time
 
-
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
 
     def __init__(self, request):
         self.request = request
+
+    def _send_confirmation_email(self, order):
+        """Send the user a confirmation email"""
+        customer_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+        
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [customer_email]
+        )        
 
     def handle_event(self, event):
         """
@@ -39,34 +58,34 @@ class StripeWH_Handler:
         for field, value in shipping_details.address.items():
             if value == "":
                 shipping_details.address[field] = None
-        
+
         # Update profile information if save_info was checked
         profile = None
         username = intent.metadata.username
-        if username != 'AnnoymousUser':
+        if username != 'AnonymousUser':
             profile = UserProfile.objects.get(user__username=username)
             if save_info:
-                profile.default_phone_number=shipping_details.phone,
-                profile.default_street_address1=shipping_details.address.street_address1,
-                profile.default_street_address2=shipping_details.address.street_address2,
-                profile.default_postcode=shipping_details.address.postal_code,
-                profile.default_town_or_city=shipping_details.address.city,
-                profile.default_country=shipping_details.address.country,
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
                 profile.save()
 
         order_exists = False
         attempt = 1
         while attempt <= 5:
             try:
-                order = Order.objects.get (
+                order = Order.objects.get(
                     full_name__iexact=shipping_details.name,
                     email__iexact=billing_details.email,
                     phone_number__iexact=shipping_details.phone,
-                    street_address1__iexact=shipping_details.address.line1,
-                    street_address2__iexact=shipping_details.address.line2,
+                    country__iexact=shipping_details.address.country,
                     postcode__iexact=shipping_details.address.postal_code,
                     town_or_city__iexact=shipping_details.address.city,
-                    country__iexact=shipping_details.address.country,
+                    street_address1__iexact=shipping_details.address.line1,
+                    street_address2__iexact=shipping_details.address.line2,
                     grand_total=grand_total,
                     original_bag=bag,
                     stripe_pid=pid,
@@ -77,8 +96,9 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
+            self._send_confirmation_email(order)
             return HttpResponse(
-                content=f'Webhook received: {event["type"]} | SUCCESS: verified order already in database',
+                content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
                 status=200)
         else:
             order = None
@@ -88,11 +108,11 @@ class StripeWH_Handler:
                     user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
-                    street_address1=shipping_details.address.line1,
-                    street_address2=shipping_details.address.line2,
+                    country=shipping_details.address.country,
                     postcode=shipping_details.address.postal_code,
                     town_or_city=shipping_details.address.city,
-                    country=shipping_details.address.country,
+                    street_address1=shipping_details.address.line1,
+                    street_address2=shipping_details.address.line2,
                     original_bag=bag,
                     stripe_pid=pid,
                 )
@@ -106,7 +126,7 @@ class StripeWH_Handler:
                         )
                         order_per_item.save()
                     else:
-                        for nicotine, quantity in item_data['items_by_nicotine'].items():
+                        for size, quantity in item_data['items_by_size'].items():
                             order_per_item = OrderPerItem(
                                 order=order,
                                 product=product,
@@ -120,6 +140,7 @@ class StripeWH_Handler:
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
+        self._send_confirmation_email(order)
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
             status=200)
