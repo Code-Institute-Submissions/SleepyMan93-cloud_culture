097b8aba3094711cd713f58f255db8e4ab87bba0
SleepyMan93 <w-donovan@hotmail.co.uk>
2021-11-18 13:30:54 +0000

    Added blog_post view and url, updated all blog template

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_blog_posts, name='blog')
+    path('', views.all_blog_posts, name='blog'),
+    path('<blog_id>', views.blog_post, name='blog_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 def all_blog_posts(request):
@@ -11,3 +11,14 @@ def all_blog_posts(request):
     }
 
     return render(request, 'blog/blog.html', context)
+
+def blog_post(request, blog_id):
+    """ A view to render individual blog posts or return 404 error page """
+
+    post = get_object_or_404(Blog, pk=blog_id)
+
+    context = {
+        'post': blog,
+    }
+
+    return render(request, 'blog/blog_post.html', context)
